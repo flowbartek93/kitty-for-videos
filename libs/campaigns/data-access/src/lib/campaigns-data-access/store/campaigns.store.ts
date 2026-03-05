d7e58afe6be9623036f737a54830a3bd5aa03ea5
BartekDev <b.ostrowski93@hotmail.com>
2026-03-05 21:13:54 +0100

    tscofnig settings

--- libs/campaigns/data-access/src/lib/campaigns-data-access/store/campaigns.store.ts
+++ libs/campaigns/data-access/src/lib/campaigns-data-access/store/campaigns.store.ts
@@ -1,8 +1,10 @@
 import { signalStore, withState } from '@ngrx/signals';
+import { withCampaignsMethods } from './campaigns.methods';
 import { campaignsState, initialCampaignsState } from './campaigns.state';
 
 export const CampaignsStore = signalStore(
   { providedIn: 'root' },
   withState(initialCampaignsState),
   campaignsState,
+  withCampaignsMethods(),
 );
