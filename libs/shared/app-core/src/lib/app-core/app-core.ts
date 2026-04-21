36f2bfbd685acb945ba390e61009f8936cff8eb3
Bartłomiej Ostrowski <b.ostrowski93@hotmail.com>
2026-04-21 14:36:19 +0200

    app core

---
+++ libs/shared/app-core/src/lib/app-core/app-core.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-app-core',
+  imports: [],
+  templateUrl: './app-core.html',
+  styleUrl: './app-core.css',
+})
+export class AppCore {}
