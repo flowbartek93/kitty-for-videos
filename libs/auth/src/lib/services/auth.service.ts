9c7c83c61bd77289dbc93189f454b3c6afd18eb6
BartekDev <b.ostrowski93@hotmail.com>
2026-04-25 11:27:41 +0200

    porzadki auth service

--- libs/auth/src/lib/services/auth.service.ts
+++ libs/auth/src/lib/services/auth.service.ts
@@ -1,18 +1,9 @@
 import { inject, Injectable } from '@angular/core';
 import { SupabaseClientService } from '@teamfund/shared';
-import { Router } from '@angular/router';
-import { AuthStore } from '../store/auth.store';
-import { Session } from '@supabase/supabase-js';
-import { from } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
   protected supabase = inject(SupabaseClientService);
-  private authStore = inject(AuthStore);
-
-  getClientAuth() {
-    return this.supabase.client.auth;
-  }
 
   login(email: string, password: string) {
     return this.supabase.client.auth.signInWithPassword({ email, password });
@@ -20,17 +11,5 @@ export class AuthService {
 
   logout() {
     return this.supabase.client.auth.signOut();
-  }
-
-  setSession(session: Session) {
-    this.authStore.setSession(session);
-  }
-
-  getStoreSession() {
-    return this.authStore.currentSession();
-  }
-
-  getSessionStorageSession() {
-    return from(this.supabase.client.auth.getSession());
   }
 }
