0c0006b554a1146907b9c0e5e939ab4681eef1f8
BartekDev <b.ostrowski93@hotmail.com>
2026-07-11 10:43:21 +0200

    walidacja configu

--- apps/special-assignments/src/app/app.controller.ts
+++ apps/special-assignments/src/app/app.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
+
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
@@ -9,4 +10,12 @@ export class AppController {
   getData() {
     return this.appService.getData();
   }
+
+
+ @Get()
+  connectSb() {
+    return this.appService.getData();
+  }
+
+
 }
--- apps/special-assignments/src/app/app.module.ts
+++ apps/special-assignments/src/app/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import Joi from 'joi';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      //praca z .env
+      envFilePath: '.env',
+      isGlobal: true,
+      validationSchema: Joi.object({ SUPABASE_URL: Joi.required(), SUPABASE_KEY: Joi.required() }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
