8119c5fa00b2ba9d37a52b77e600f5e4545bda5e
Bartłomiej Ostrowski <b.ostrowski93@hotmail.com>
2026-07-14 19:22:25 +0200

    supabase module

--- apps/special-assignments/src/app/app.module.ts
+++ apps/special-assignments/src/app/app.module.ts
@@ -1,23 +1,8 @@
 import { Module, Provider } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import Joi from 'joi';
+import { SupabaseModule } from '@teamfund/backend-supabase';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
-// const supapaseProvider: Provider = {
-//   provide: 'SUPABASE_CLIENT',
-//   useFactory: (configSrv: ConfigService) => {
-//     const url = configSrv.get<string>('SUPABASE_URL');
-//     const key = configSrv.get<string>('SUPABASE_SERVICE_ROLE_KEY');
-
-//     if (!url || !key) {
-//       throw new Error('CRITICAL BREACH: Supabase env credentials completely unreadable.');
-//     }
-
-//     return createClient(url, key, {});
-//   },
-//   inject: [ConfigService],
-// };
 
 @Module({
   imports: [
@@ -26,6 +11,14 @@ import { AppService } from './app.service';
       envFilePath: '.env',
       isGlobal: true,
       // validationSchema: Joi.object({ SUPABASE_URL: Joi.required(), SUPABASE_KEY: Joi.required() }),
+    }),
+    SupabaseModule.forRootAsync({
+      useFactory: (config: ConfigService) => {
+        return {
+          serviceRoleKey: config.getOrThrow('SUPABASE_SERVICE_ROLE_KEY'),
+          url: config.getOrThrow('SUPABASE_URL'),
+        };
+      },
     }),
   ],
   controllers: [AppController],
--- libs/backend/supabase/src/lib/supabase.module.ts
+++ libs/backend/supabase/src/lib/supabase.module.ts
@@ -1,8 +1,34 @@
-import { Module } from '@nestjs/common';
+import { ConfigurableModuleBuilder, Module, Provider } from '@nestjs/common';
+import { SUPABASE_CLIENT } from './shared/supabase.const';
+import { createClient } from '@supabase/supabase-js';
+
+export interface SupabaseModuleOptions {
+  url: string;
+  serviceRoleKey: string;
+}
+
+export const { MODULE_OPTIONS_TOKEN, ConfigurableModuleClass } = new ConfigurableModuleBuilder<SupabaseModuleOptions>()
+  .setClassMethodName('forRoot')
+  .build();
+
+const supapaseProvider: Provider = {
+  provide: SUPABASE_CLIENT,
+  useFactory: (options: SupabaseModuleOptions) => {
+    const url = options.url;
+    const key = options.serviceRoleKey;
+
+    if (!url || !key) {
+      throw new Error('CRITICAL BREACH: Supabase env credentials completely unreadable.');
+    }
+
+    return createClient(url, key, {});
+  },
+  inject: [MODULE_OPTIONS_TOKEN],
+};
 
 @Module({
   controllers: [],
-  providers: [],
+  providers: [supapaseProvider],
   exports: [],
 })
-export class SupabaseModule {}
+export class SupabaseModule extends ConfigurableModuleClass {}
